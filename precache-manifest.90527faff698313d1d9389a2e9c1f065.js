a790554b8fa24a215ad4cca694cd4552ed43f7c7
Richard Norman <richard.norman.id@gmail.com>
2020-07-22 14:19:55 -0600

    Updates

---
+++ precache-manifest.90527faff698313d1d9389a2e9c1f065.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa7d6088d2c6712d1fe0914632be69aa",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9b1acf91a55743bfef51",
+    "url": "/robofriends/static/css/main.992405bc.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1acf91a55743bfef51",
+    "url": "/robofriends/static/js/main.3c667cea.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
